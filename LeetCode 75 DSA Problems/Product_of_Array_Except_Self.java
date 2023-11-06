331a262619e4e97dc72a5007c525497c3d75982a
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-06 23:27:33 +0530

    Create Product_of_Array_Except_Self.java

---
+++ LeetCode 75 DSA Problems/Product_of_Array_Except_Self.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+class Product_of_Array_Except_Self {
+    public int[] productExceptSelf(int[] nums) {
+        Set <Integer> set=new HashSet<>;
+        for(int num:nums)
+        {
+            set.add(num);
+        }
+        for(int set1:set)
+        {
+            set1 =set*set1;
+        }
+        return set1;
+    }
+}
