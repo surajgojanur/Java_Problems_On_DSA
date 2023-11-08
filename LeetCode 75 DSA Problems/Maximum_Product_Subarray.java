e54007c20acb72bab45ca9a3ce78d5532705eeb3
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-08 23:37:00 +0530

    Create Maximum_Product_Subarray.java

---
+++ LeetCode 75 DSA Problems/Maximum_Product_Subarray.java
@@ -0,0 +1,26 @@
+class Maximum_Product_Subarray {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) {
+            return 0; // Handle the case when the array is empty
+        }
+
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
+            minProduct = Math.min(nums[i], minProduct * nums[i]);
+
+            result = Math.max(result, maxProduct);
+        }
+
+        return result;
+    }
+}
