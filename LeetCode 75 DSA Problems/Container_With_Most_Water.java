94c1d7b347952c8c9f149a7f8e94c4c53991fdb1
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-17 23:47:11 +0530

    Update Container_With_Most_Water.java

--- LeetCode 75 DSA Problems/Container_With_Most_Water.java
+++ LeetCode 75 DSA Problems/Container_With_Most_Water.java
@@ -1,5 +1,13 @@
 class Container_With_Most_Water{
     public int maxArea(int[] height){
+        int max=height[0];
+        int max2=height[1];
+        int temp=0;
+        for(int i=0;i<height.length;i++){
+            if(max<height[i]){
+                temp=max;
+                max=height[i];
+                max2=temp;
 
         
     }
