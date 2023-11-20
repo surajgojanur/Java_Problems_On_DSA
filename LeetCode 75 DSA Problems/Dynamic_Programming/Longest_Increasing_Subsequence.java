9eecb2efce3bcc3fa46f8a27b8c49e2b45627a2a
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-21 02:34:51 +0530

    Update Longest_Increasing_Subsequence.java

--- LeetCode 75 DSA Problems/Dynamic_Programming/Longest_Increasing_Subsequence.java
+++ LeetCode 75 DSA Problems/Dynamic_Programming/Longest_Increasing_Subsequence.java
@@ -1,19 +1,20 @@
 class Longest_Increasing_Subsequence{
     public int lengthOfLIS(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
         int[] dp=new int[nums.length];
-        int result=0;
-
-        for(int i=nums.length-1;i>=0;i--){
-            int count=0;
-            for(int j=i-1;j>=0;j--){
+        Arrays.fill(dp,1);
+        int maxLIS=1;
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
                 if(nums[i]>nums[j]){
-                    count++;
+                    dp[i]=Math.max(dp[i],dp[j]+1);
                 }
             }
-            
-            result=Math.max(count,result);
+            maxLIS=Math.max(maxLIS,dp[i]);
         }
-        return result;
+        return maxLIS;
         
     }
 }
