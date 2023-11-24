b866e7ed3092941dda40fdc5648d414fc523a203
Suraj Gojanur <surajgojanur@gmail.com>
2023-11-25 00:07:40 +0530

    Create Unique_ways.java

---
+++ LeetCode 75 DSA Problems/Dynamic_Programming/Unique_ways.java
@@ -0,0 +1,16 @@
+class Unique_ways{
+    public int uniquePath(int m,int n){
+        int[][] dp=new dp[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
