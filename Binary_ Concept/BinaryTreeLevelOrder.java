cf57d1afa6a348e77e1ad72a08a78834382ff528
Suraj Gojanur <surajgojanur@gmail.com>
2023-12-02 22:48:59 +0530

    Create BinaryTreeLevelOrder.java

---
+++ Binary_ Concept/BinaryTreeLevelOrder.java
@@ -0,0 +1,57 @@
+class BinaryTreeLevelOrder{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data1{
+            this.data=data;
+            this.right=null;
+            this.left=null;            
+        })
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return;
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+
+            return;
+        }
+    }
+    public static void levelorder(Node root){
+        if(root==null){
+            return;
+        }
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node curr=q.remove();
+
+            if(curr==null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else{q.add(null);
+                }
+                else{
+                    System.out.print(curr.data+" ");
+                    if(curr.left!=null){
+                        q.add(curr.left);
+                    }
+                    if(curr.right!=null){
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
+    }
+}
