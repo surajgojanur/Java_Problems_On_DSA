b1feaa01875afe78bbaaf1f4559afac5058f2926
Suraj Gojanur <surajgojanur@gmail.com>
2023-12-04 20:15:46 +0530

    Create BinaryTreeHeightAndDiagonal.java

---
+++ Binary_ Concept/BinaryTreeHeightAndDiagonal.java
@@ -0,0 +1,41 @@
+public class BinaryTreeHeightAndDiagonal{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+
+            return newNode;
+        }
+    }
+    public static TreeInfo diameter(Node root){
+        if(root==null){
+            return new TreeInfo(0,0);
+        }
+        TreeInfo leftTI=diameter(root.left);
+        TreeInfo rightTI=
+    }
+    public static void main(String args[]){
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root=tree.buildTree(nodes);
+
+    }
+}
