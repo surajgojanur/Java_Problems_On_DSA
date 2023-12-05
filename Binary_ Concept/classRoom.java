e32308d8c8588b0f3730df5cfce68ece0bda9bab
Suraj Gojanur <surajgojanur@gmail.com>
2023-12-05 23:01:44 +0530

    Create classRoom.java

---
+++ Binary_ Concept/classRoom.java
@@ -0,0 +1,13 @@
+public class classRoom{
+    static class Edge{
+        int src;
+        int dest;
+    }
+    public Edge(int s, int d){
+        this.src=s;
+        this.dest=d;
+    }
+}
+public static void createGraph(ArrayList<Edge> graph){
+    
+}
