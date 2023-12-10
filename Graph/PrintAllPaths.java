0a0003334c22d2f8f62e9fd97d6e9e4bfee5ad9f
Suraj Gojanur <surajgojanur@gmail.com>
2023-12-10 23:04:42 +0530

    Create PrintAllPaths.java

---
+++ Graph/PrintAllPaths.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class PrintAllPaths{
+    static class Edge{
+        int src;
+        int dest;
+        public Edge(int s,int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]
+        }
+    }
+}
