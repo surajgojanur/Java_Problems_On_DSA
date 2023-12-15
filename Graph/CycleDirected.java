34944449a9ffe09c9c4a3b030b62e60e1dbaa103
Suraj Gojanur <surajgojanur@gmail.com>
2023-12-15 13:59:48 +0530

    Create CycleDirected.java

---
+++ Graph/CycleDirected.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CycleDirected{
+    static class Edge{
+        int src;
+        int dest;
+        public Edge(int s,int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+    static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<>();
+        }
+        graph[0].add(new Edge(0,2));
+        graph[1].add(new Edge(1,0));
+        graph[2].add(new Edge(2,3));
+        graph[3].add(new Edge(3,0));
+    }
+}
