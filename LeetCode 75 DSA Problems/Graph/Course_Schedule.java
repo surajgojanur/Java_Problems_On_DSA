df3a7003771173ae43b4253eda1cbebaa27b6f32
Suraj Gojanur <surajgojanur@gmail.com>
2024-01-02 23:24:54 +0530

    v

--- LeetCode 75 DSA Problems/Graph/Course_Schedule.java
+++ LeetCode 75 DSA Problems/Graph/Course_Schedule.java
@@ -1,20 +1,34 @@
-public class Course_Schedule{
-    public class Solution{
-        public boolean canFinish(int numCourse,int[][] prerequisities){
+classCourse_Schedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
             List<List<Integer>> adjList=new ArrayList<>();
-            for(int[] prerequisitie:prerequisities){
+            for(int[] prerequisitie:prerequisites){
                 adjList.get(prerequisitie[1]).add(prerequisitie[0]);
             }
             //To Track Visted nodes
-            int[] Visted=new int[numCourse];
+            int[] Visted=new int[numCourses];
 
-            for(int i=0;i<numCourse;i++){
+            for(int i=0;i<numCourses;i++){
                 if(hasCycle(adjList,Visted,i)){
                     return false;
                 }
             }
             return true;
         }
-        private
+        private boolean hasCycle(List<List<Integer>> adjList,int[] Visted,int course){
+            if(Visted[course]==1){
+                return true;
+            }
+            if(Visted[course]==2){
+                return false;
+            }
+            Visted[course]=1;
+
+            for(int nextCourse:adjList.get(course)){
+                if(hasCycle(adjList,Visted,nextCourse)){
+                    return true;
+                }
+            }
+            Visted[course]=2;
+            return false;
+        }
     }
-}
