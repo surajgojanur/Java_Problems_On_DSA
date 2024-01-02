7875ec1e210247d68635ac271242b51c5ebe5639
Suraj Gojanur <surajgojanur@gmail.com>
2024-01-02 22:18:34 +0530

    Node

--- LeetCode 75 DSA Problems/Graph/Course_Schedule.java
+++ LeetCode 75 DSA Problems/Graph/Course_Schedule.java
@@ -1,5 +1,20 @@
 public class Course_Schedule{
     public class Solution{
-        public boolean canFinish(int numCourse )
+        public boolean canFinish(int numCourse,int[][] prerequisities){
+            List<List<Integer>> adjList=new ArrayList<>();
+            for(int[] prerequisitie:prerequisities){
+                adjList.get(prerequisitie[1]).add(prerequisitie[0]);
+            }
+            //To Track Visted nodes
+            int[] Visted=new int[numCourse];
+
+            for(int i=0;i<numCourse;i++){
+                if(hasCycle(adjList,Visted,i)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        private
     }
 }
