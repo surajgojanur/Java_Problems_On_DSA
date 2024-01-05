0f2d345dd587b8da1f1a095f36ed2ea5a078bc1e
Suraj Gojanur <surajgojanur@gmail.com>
2024-01-05 18:17:49 +0530

    Create Number_of_Islands.java

---
+++ LeetCode 75 DSA Problems/Graph/Number_of_Islands.java
@@ -0,0 +1,35 @@
+class Number_of_Islands {
+    public int numIslands(char[][] grid) {
+        if(grid==null || grid.length==0){
+            return 0;
+        }
+        int count=0;
+        int rows=grid.length;
+        int cols=grid[0].length;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(char[][] grid, int row, int col) {
+    int rows = grid.length;
+    int cols = grid[0].length;
+
+    if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] != '1') {
+        return;
+    }
+    grid[row][col] = '0';
+
+    // Explore adjacent cells
+    dfs(grid, row + 1, col);
+    dfs(grid, row - 1, col);
+    dfs(grid, row, col + 1);
+    dfs(grid, row, col - 1);
+}
+}
