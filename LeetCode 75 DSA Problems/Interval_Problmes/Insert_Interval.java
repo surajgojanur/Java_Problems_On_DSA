89af49d0651485121faa7c2bf03c4c6403eb9958
Suraj Gojanur <surajgojanur@gmail.com>
2024-01-09 00:40:56 +0530

    Update Insert_Interval.java

--- LeetCode 75 DSA Problems/Interval_Problmes/Insert_Interval.java
+++ LeetCode 75 DSA Problems/Interval_Problmes/Insert_Interval.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Insert_Interval{
-    public int[][] insert(int[][] intervlas,int newInterval)
+    public int[][] insert(int[][] intervals,int newInterval){
+    List<int[]> result=new ArrayList<>();
+    int newStart=newInterval[0];
+    int newEnd=newInterval[1];
+    int i=0;
+    int n=intervals.length;
+
+    while(i<n && intervals[i][1]<newStart){
+        result.add(intervals[i]);
+        i++;
+    }
+
+    while (i<n && intervals[i][0]<= newEnd) {
+        newStart=Math.min(newStart,intervals[i][0]);
+        newEnd=Math.max(newEnd,intervals[i][1]);
+        i++;
+    }
+    result.add(new int[]{newStart,newEnd});
+
+    while (i<n) {
+        result.add(intervals[i]);
+        i++;
+    }
+    return result.toArray(new int[result.size()][]);
+    }
 }
