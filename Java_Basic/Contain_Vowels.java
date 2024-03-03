942ea56c12ac77f99168f3e12f7e686faa6741a5
Suraj Gojanur <surajgojanur@gmail.com>
2024-03-03 13:35:15 +0530

    s

---
+++ Java_Basic/Contain_Vowels.java
@@ -0,0 +1,22 @@
+class Contain_Vowels{
+
+    public void check(String a){
+        char[] ch=a.toCharArray();
+        for(int i=0;i<a.length();i++){
+            if(ch[i]=='a' || ch[i]=='e' ||ch[i]=='i' || ch[i]=='o' || ch[i]=='u' ){
+                System.out.println("Yes it contains Vowels = "+a);
+                break;
+            }
+        }
+        System.out.println("False it does not contain Vowels");
+    }
+    public static void main(String args[]){
+
+    String a="Suraj";
+    Contain_Vowels obja=new Contain_Vowels();
+    obja.check(a);
+
+
+
+    }
+}
