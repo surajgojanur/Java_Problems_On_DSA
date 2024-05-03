04dd49eede2b27ac01e64645758f60695ab5bd7d
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-05-03 23:00:29 +0530

    1

--- Java_Basic/Swap_a_number_without_thrid_no.java
+++ Java_Basic/Swap_a_number_without_thrid_no.java
@@ -1,16 +1,17 @@
 class Swap_a_number_without_thrid_no{
     public void swap(int a,int b){
-        int b=b+a;
-        int a=b-a;
-        int b=b-a;
+        b=b+a;
+        a=b-a;
+        b=b-a;
 
         System.out.println(a+" "+b);
 
     }
     public static void main(String args[]){
         Swap_a_number_without_thrid_no st=new Swap_a_number_without_thrid_no();
-        int a,b=1,2;
+        int a=1;
+        int b=2;
         System.out.println(a+" "+b);
-        Swap_a_number_without_thrid_no.swap(a,b);
+        st.swap(a,b);
     }
 }
