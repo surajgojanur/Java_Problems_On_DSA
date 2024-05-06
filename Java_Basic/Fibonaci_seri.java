47291e450177278412932a953c7245cbe0dbe275
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-05-06 22:36:40 +0530

    Create Fibonaci_seri.java

---
+++ Java_Basic/Fibonaci_seri.java
@@ -0,0 +1,11 @@
+import java.Scanner;
+class Fibonaci_seri{
+    public void fibo(int n){
+
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enterr a number :");
+        int n=sc.nextInt();
+    }
+}
