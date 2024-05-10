bb2288cd479152f81a8b5ffc9fdd4eca4adef88e
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-05-10 21:46:42 +0530

    Update spiralOrder.java

--- LeetCode 75 DSA Problems/Matrix/spiralOrder.java
+++ LeetCode 75 DSA Problems/Matrix/spiralOrder.java
@@ -1,4 +1,42 @@
-
-public class spiralOrder {
-    //spiralOrder
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> res=new ArrayList<>();
+        if(matrix==null){
+            return res;
+        }
+        int m=matrix.length;
+        if(m==0){
+            return res;
+        }
+        int n=matrix[0].length;
+        int l=0,r=n-1,t=0,b=m-1;
+        while(t<=b && l<=r){
+            for(int i=l;i<=r;i++){
+                res.add(matrix[t][i]);
+            }
+            t++;
+            if(t>b){
+                break;
+            }
+            for(int i=t;i<=b;i++){
+                res.add(matrix[i][r]);
+            }
+            r--;
+            if(r<l){
+                break;
+            }
+            for(int i=r;i>=l;i--){
+                res.add(matrix[b][i]);
+            }
+            b--;
+            if(b<t){
+                break;
+            }
+            for(int i=b;i>=t;i--){
+                res.add(matrix[i][l]);
+            }
+            l++;
+        }
+        return res;
+    }
 }
