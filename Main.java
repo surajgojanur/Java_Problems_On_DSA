326bedfc16d35c48d30e41b9a1d2d2e12f65f58d
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-05-16 22:39:11 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n = sc.nextInt();
-        //int n2=n/2;
+        
         if(n>0){
             System.out.println("Postive");
         }
