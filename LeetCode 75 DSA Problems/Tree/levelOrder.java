31af075414a34c765e717c1edc42488a26e75aef
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-06-18 01:54:56 +0530

    Update levelOrder.java

--- LeetCode 75 DSA Problems/Tree/levelOrder.java
+++ LeetCode 75 DSA Problems/Tree/levelOrder.java
@@ -13,28 +13,27 @@
  *     }
  * }
  */
-// Binary Tree Level Order Traversalclass Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<Node> q=new LinkedList<>();
-        q.add(root);
-        q.add(null);
-        List<List<Integer>> lt=new ArrayList<>();
-        while(!q.isEmpty()){
-            Node curr=q.pull();
-            if(curr==null){
-                if(q.isEmpty()){
-                    return lt;
-                }
-                q.add(null);
-
-            }
-
-            if(curr.left != null){
-                q.add(curr.left);
-            }
-            if(curr.right != null){
-                q.add(curr.right);
-            }
-        }
-        return lt;
+// Binary Tree Level Order Traversal
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) 
+    {
+        List<List<Integer>>al=new ArrayList<>();
+        pre(root,0,al);
+        return al;
     }
+    public static void pre(TreeNode root,int l,List<List<Integer>>al)
+    {
+        if(root==null)
+            return;
+        if(al.size()==l)
+        {
+            List<Integer>li=new ArrayList<>();
+            li.add(root.val);
+            al.add(li);
+        }
+        else
+            al.get(l).add(root.val);
+        pre(root.left,l+1,al);
+        pre(root.right,l+1,al);
+    } 
+}
