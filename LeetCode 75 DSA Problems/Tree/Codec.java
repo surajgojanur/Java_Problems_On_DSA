967e223ba54a2c421cfffc5e1826c651c766ba73
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-06-20 03:00:32 +0530

    Update Codec.java

--- LeetCode 75 DSA Problems/Tree/Codec.java
+++ LeetCode 75 DSA Problems/Tree/Codec.java
@@ -34,20 +34,23 @@
             if(data=="")return null;
             Queue<TreeNode> q=new LinkedList<>();
             String[] values=data.split(" ");
-            TreeNode root=new TreeNode(Integer.paraseInt(values[0]));
+            TreeNode root=new TreeNode(Integer.parseInt(values[0]));
             q.add(root);
 
             for(int i=1;i<values.length;i++){
                 TreeNode parent=q.poll();
                 if(!values[i].equals("n")){
-                    TreeNode left=new TreeNode(Integer.paraseInt(values[i]));
+                    TreeNode left=new TreeNode(Integer.parseInt(values[i]));
                     parent.left=left;
                     q.add(left);
                 }
                 if(!values[++i].equals("n")){
-                    TreeNode right
+                    TreeNode right=new TreeNode(Integer.parseInt(values[i]));
+                    parent.right=right;
+                    q.add(right);
                 }
             }
+            return root;
         }
     }
 
