037a7b1e51e726b1b14daff70758f789fb044d6f
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-06-20 02:32:06 +0530

    Create Codec.java

---
+++ LeetCode 75 DSA Problems/Tree/Codec.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if(root==null) return "";
+        Queue<TreeNode> q=new LinkedList<>();
+        StringBuilder res=new StringBuilder();
+        
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
