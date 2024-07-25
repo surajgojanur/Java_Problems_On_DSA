02982bf0b14e3f236ba4094b0e1a5a33e1cd08b2
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-07-25 22:25:01 +0530

    Update maxProfit.java

--- LeetCode 75 DSA Problems/Array_Problems/maxProfit.java
+++ LeetCode 75 DSA Problems/Array_Problems/maxProfit.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int tn=0;
-        for (int i=0;i<prices.length-1;i++)
-        {
-            if(prices[i]<prices[i+1])
-            {
-                tn+=prices[i]+prices[i+1];
-                return tn;
+        int buy=prices[0];
+        int profit=0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i+1]<buy){
+                buy=prices[i+1];
+            }
+            else if(prices[i+1]-buy > profit){
+                profit=prices[i+1]-buy;
             }
         }
-        return tn;
-        
+        return profit;
     }
 }
