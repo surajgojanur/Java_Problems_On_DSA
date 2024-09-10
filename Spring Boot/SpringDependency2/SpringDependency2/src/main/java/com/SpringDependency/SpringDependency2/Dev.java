f8d01d0238374626c34de99904e36c7758acf833
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-09-10 16:23:07 +0530

    2

---
+++ Spring Boot/SpringDependency2/SpringDependency2/src/main/java/com/SpringDependency/SpringDependency2/Dev.java
@@ -0,0 +1,10 @@
+package com.SpringDependency.SpringDependency2;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public void build(){
+        System.out.printf("Working on Basic Stuffff");
+    }
+}
--- Spring Boot/SpringDependency2/SpringDependency2/src/main/java/com/SpringDependency/SpringDependency2/SpringDependency2Application.java
+++ Spring Boot/SpringDependency2/SpringDependency2/src/main/java/com/SpringDependency/SpringDependency2/SpringDependency2Application.java
@@ -2,12 +2,17 @@ package com.SpringDependency.SpringDependency2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDependency2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDependency2Application.class, args);
+		ApplicationContext context = SpringApplication.run(SpringDependency2Application.class, args);
+		Dev obj = context.getBean(Dev.class);
+		obj.build();
 	}
 
+
 }
+
