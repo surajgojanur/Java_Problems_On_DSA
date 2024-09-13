3a1023b7a721a740655a079e755db1d1d8eba056
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-09-13 19:50:14 +0530

    Update Laptop.java

--- Spring Boot/SpringDependency2/SpringDependency2/src/main/java/com/SpringDependency/SpringDependency2/Laptop.java
+++ Spring Boot/SpringDependency2/SpringDependency2/src/main/java/com/SpringDependency/SpringDependency2/Laptop.java
@@ -1,8 +1,10 @@
 package com.SpringDependency.SpringDependency2;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Laptop implements Computer {
     public void complie(){
         System.out.println("Laptop !!!");
