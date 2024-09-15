0b00c6d0c7e6b7dfa64ab1c540438ab9b2b266f7
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-09-15 16:21:55 +0530

    xml update

--- Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/App.java
+++ Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/App.java
@@ -13,7 +13,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Dev obj  = context.getBean(Dev.class);
         obj.build();
 
