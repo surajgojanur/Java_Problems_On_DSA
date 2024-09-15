cd232bc98dcad5fa1ab6d487490126e344217ed4
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-09-15 16:58:27 +0530

    Create Laptop.java

---
+++ Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/Laptop.java
@@ -0,0 +1,10 @@
+package org.suraj;
+
+public class Laptop {
+    public Dev(){
+        System.out.printf("This is Laptop constrctor");
+    }
+    public void complie(){
+        System.out.printf("THis is Laptop ");
+    }
+}
