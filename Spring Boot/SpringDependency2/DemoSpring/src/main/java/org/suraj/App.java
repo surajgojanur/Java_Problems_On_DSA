dd3fa1ff1e314d0b1679813a4390bb19c58b9441
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-09-17 23:33:22 +0530

    Update App.java

--- Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/App.java
+++ Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/App.java
@@ -11,6 +11,7 @@ public class App
 {
 
     public static void main( String[] args )
+            //suraj
     {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
