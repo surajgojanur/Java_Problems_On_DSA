875de3a8308598febcab5988b0659d03b0f5de09
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2024-09-28 20:53:06 +0530

    Disk_Format_Guide

--- Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/Dev.java
+++ Spring Boot/SpringDependency2/DemoSpring/src/main/java/org/suraj/Dev.java
@@ -1,10 +1,32 @@
 package org.suraj;
 
 public class Dev {
+    private Laptop laptop;
+    private int age;
     public Dev(){
         System.out.println("DEV");
     }
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+    public Laptop setLaptop(){
+        this.laptop=laptop;
+    }
+    public Dev(Laptop laptop){
+        this.laptop=laptop;
+        System.out.println("DEv 1 Con");
+    }
+
     public void build(){
-        System.out.printf("Working on Spring with out SPring Boot");
+        System.out.println("Working on Spring with out SPring Boot");
+        laptop.complie();
     }
 }
