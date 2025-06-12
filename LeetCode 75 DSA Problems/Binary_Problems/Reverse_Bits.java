9b62a78841f7ceed1805d443188577c87cc713ae
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2025-06-12 15:36:17 +0530

    Update Reverse_Bits.java

--- LeetCode 75 DSA Problems/Binary_Problems/Reverse_Bits.java
+++ LeetCode 75 DSA Problems/Binary_Problems/Reverse_Bits.java
@@ -1,13 +1,20 @@
-public class Reverse_Bits {
-    // you need treat n as an unsigned value
+public class Solution {
+    // you need to treat n as an unsigned value
     public int reverseBits(int n) {
-        int result=0;
-
-        for(int i=0;i<32;i++){
-            result=result<<1 | (n&1);
-            n>>=1;
-        }
-        return result;
+        int result = 0;
         
+        // Iterate over all 32 bits of the integer
+        for (int i = 0; i < 32; i++) {
+            // Shift result to the left to make space for the next bit
+            result <<= 1;
+
+            // Take the last bit of n and add it to result
+            result |= (n & 1);
+
+            // Unsigned right shift n to discard the bit we just used
+            n >>>= 1;
+        }
+        
+        return result;
     }
 }
