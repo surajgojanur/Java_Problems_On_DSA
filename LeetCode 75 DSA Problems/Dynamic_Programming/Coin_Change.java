ea02d59b0d56613d3bf587bf0e31162c751fe0f8
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2025-06-13 16:26:11 +0530

    Update Coin_Change.java

--- LeetCode 75 DSA Problems/Dynamic_Programming/Coin_Change.java
+++ LeetCode 75 DSA Problems/Dynamic_Programming/Coin_Change.java
@@ -1,16 +1,16 @@
-class Coin_Change {
+class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
 
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        for(int i=0;i<=amount;i++){
+            for(int coin:coins){
+                if(i-coin >= 0){
+                    dp[i]=Math.min(dp[i],1+dp[i-coin]);
+                }
             }
         }
-
-        return dp[amount] > amount ? -1 : dp[amount];
-        
+        return dp[amount]==amount+1 ? -1 : dp[amount];
     }
 }
