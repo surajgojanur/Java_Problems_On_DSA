bb06f8388772db98c7f31d92ce8c09423857a1ed
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2025-06-14 22:09:01 +0530

    Update Longest_Increasing_Subsequence.java

--- LeetCode 75 DSA Problems/Dynamic_Programming/Longest_Increasing_Subsequence.java
+++ LeetCode 75 DSA Problems/Dynamic_Programming/Longest_Increasing_Subsequence.java
@@ -1,20 +1,22 @@
-class Longest_Increasing_Subsequence{
+class Solution {
     public int lengthOfLIS(int[] nums) {
-        if(nums.length==0){
+        int n=nums.length;
+        if(n==0){
             return 0;
         }
-        int[] dp=new int[nums.length];
+        int[] dp=new int[n];
         Arrays.fill(dp,1);
-        int maxLIS=1;
-        for(int i=1;i<nums.length;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
+                if(nums[j]<nums[i]){
                     dp[i]=Math.max(dp[i],dp[j]+1);
                 }
             }
-            maxLIS=Math.max(maxLIS,dp[i]);
         }
-        return maxLIS;
-        
+        int result=0;
+        for(int dpi:dp){
+            result=Math.max(dpi,result);
+        }
+        return result;
     }
 }
