208b840782b1dfc30801d576be1694164ab11748
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2025-06-18 16:49:21 +0530

    Update House_Robber2.java

--- LeetCode 75 DSA Problems/Dynamic_Programming/House_Robber2.java
+++ LeetCode 75 DSA Problems/Dynamic_Programming/House_Robber2.java
@@ -1,24 +1,21 @@
-class House_Robber2 {
+class Solution {
     public int rob(int[] nums) {
-        if(nums.length==0){
-            return 0;
-        }
-        else if(nums.length==1){
-            return nums[0];
-        }
-        int max1=robHelper(nums,0,nums.length-2);
-        int max2=robHelper(nums,1,nums.length-1);
-        return Math.max(max1,max2);
-    }
-    private int robHelper(int[] nums,int start,int end){
-        int prevMax=0;
-        int currMax=0;
+        int n=nums.length;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return nums[0]>nums[1] ? nums[0] : nums[1];
+        int rb1=robbing(nums,1,n-1);
+        int rb2=robbing(nums,0,n-2);
+        return rb1>rb2?rb1:rb2;
 
+    }
+    public int robbing(int[] nums,int start,int end){
+        int pr1=0,pr2=0;
         for(int i=start;i<=end;i++){
-            int temp=currMax;
-            currMax=Math.max(prevMax+nums[i],currMax);
-            prevMax=temp;
+            int cur=Math.max(pr1,pr2+nums[i]);
+            pr2=pr1;
+            pr1=cur;
         }
-        return currMax;
+        return pr1;
     }
 }
