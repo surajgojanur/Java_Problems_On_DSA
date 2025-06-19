be0800ece896a0295d023fbcec4050cf26e76055
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2025-06-19 23:24:10 +0530

    Update Decode_Ways.java

--- LeetCode 75 DSA Problems/Dynamic_Programming/Decode_Ways.java
+++ LeetCode 75 DSA Problems/Dynamic_Programming/Decode_Ways.java
@@ -1,19 +1,21 @@
-class Decode_Ways {
+class Solution {
     public int numDecodings(String s) {
-        if(s.length() == 0 || s.charAt(0) == '0') return 0;
-        if(s.length() == 1) return 1;
-        
-        int count1 = 1, count2 = 1;
-        for(int i = 1; i < s.length(); ++i){
-            int d = s.charAt(i) - '0';
-            int dd = (s.charAt(i-1) - '0')*10 + d;
-            int count = 0;
-            if(d > 0) count += count2;
-            if(dd >= 10 && dd <= 26) count += count1;
-            count1 = count2;  // prev to prev
-            count2 = count;  // count2 prev sol
+        if(s.length()==0 || s.charAt(0)=='0' || s==null) return 0;
+        int[] dp=new int[s.length()+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=s.length();i++){
+            int oneDig=Integer.parseInt(s.substring(i-1,i));
+            int twoDig=Integer.parseInt(s.substring(i-2,i));
+
+            if(oneDig>=1 && oneDig<=9){
+                dp[i]+=dp[i-1];
+            }
+            if(twoDig>=10 && twoDig<=26){
+                dp[i]+=dp[i-2];
+            }
+
         }
-        return count2;
-        //return
+        return dp[s.length()];
     }
 }
