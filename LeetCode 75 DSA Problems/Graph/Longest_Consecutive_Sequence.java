66be517b00940e18419d079673594e3d96484174
Suraj Gojanur <85693124+surajgojanur@users.noreply.github.com>
2025-07-02 18:14:15 +0530

    Update Longest_Consecutive_Sequence.java

--- LeetCode 75 DSA Problems/Graph/Longest_Consecutive_Sequence.java
+++ LeetCode 75 DSA Problems/Graph/Longest_Consecutive_Sequence.java
@@ -1,18 +1,26 @@
-class Longest_Consecutive_Sequence{
-    public int longestConsective(int[] nums){
-        if(nums.length==0)return 0;
-        Arrays.sort(nums);
-        int max=-1;
-        int cnt=0;
-
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]==nums[i])continue;
-            else if(nums[i]-nums[i-1]==1)cnt++;
-            else{
-                if(cnt>max)max=cnt;
-            }
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0){
+            return 0;
         }
-        if(cnt>max)max=cnt;
-        return max+1;
+        else if(nums.length==1){
+            return 1;
+        }
+        Arrays.sort(nums);
+        int currentStrek=1;
+        int longestStrek=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                continue;
+            }
+            else if(nums[i]==nums[i-1]+1){
+                currentStrek++;
+            }
+            else{
+                currentStrek=1;
+            }
+            longestStrek=Math.max(currentStrek,longestStrek);
+        }
+        return longestStrek;
     }
 }
